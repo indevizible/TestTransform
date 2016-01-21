17f642b8e4681d94d6c7dba4efbabdc8e412849c
Nattawut Singhchai <jadedragon17650@gmail.com>
2016-01-21 16:05:03 +0700

    initial commit

---
+++ TestTransform/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestTransform
+//
+//  Created by Nattawut Singhchai on 1/21/16.
+//  Copyright © 2016 Nattawut Singhchai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sampleView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var perspectiveTransform = CATransform3DIdentity
+        perspectiveTransform.m34 = 1.0/(-1000)
+        perspectiveTransform = CATransform3DTranslate(perspectiveTransform, 50, 0, 0)
+        sampleView.layer.transform = perspectiveTransform
+    }
+}
+
